85af078d9cf6ac5eec36bec8eea883393db258cc
Mahijith <menon.mahijith@gmail.com>
2022-10-13 11:15:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var btn=document.querySelector('.btn');
+var txtInput=document.querySelector('.txtareainput');
+var outputDiv = document.querySelector("#output");
+var serverURL="https://api.funtranslations.com/translate/pirate.json";
+
+function getTranslationURL(text){
+    return serverURL +"?"+"text="+text;
+}
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+function clickHandler(){
+    var input=txtInput.value;
+    
+
+    fetch(getTranslationURL(input))
+        .then(response=>response.json())
+        .then(json =>{
+            var translated =json.contents.translated
+            outputDiv.innerText = translated;
+            outputDiv.style.color = "white";
+        })   
+        .catch(errorHandler)
+};
+btn.addEventListener("click",clickHandler)
